6baefd619ca57faab4f2bfe7ecc536b1e21298d0
wh_shao <haibusoi@gmail.com>
2019-10-15 10:52:12 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.use(VueFullPage);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
